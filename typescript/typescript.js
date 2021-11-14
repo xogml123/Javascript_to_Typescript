967207e490e5571dd7bc39c6f0a0ac645fbba035
xogml123 <xogml951@gmail.com>
2021-11-15 04:54:55 +0900

    Add Type Annotation

---
+++ typescript/typescript.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.forModule = void 0;
+/*
+숫자 두개를 매개변수로 받아 곱을 리턴하는 함수를 만들고 싶다.
+*/
+exports.forModule = 4;
+function multiply(x, y) {
+    return x * y;
+}
+var numbers = multiply(3, 6);
+//const strings = multiply('a', 'b');
+console.log(numbers);
+//console.log(strings);
---
+++ typescript/typescript.ts
@@ -0,0 +1,13 @@
+/*
+숫자 두개를 매개변수로 받아 곱을 리턴하는 함수를 만들고 싶다.
+*/
+export const forModule = 4;
+
+function multiply(x: number, y: number) {
+  return x * y;
+}
+
+let numbers = multiply(3, 6);
+//const strings = multiply('a', 'b');
+console.log(numbers);
+//console.log(strings);
