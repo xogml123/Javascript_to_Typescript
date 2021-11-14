1f6d265fa9a9b12282f60965d4786d4dbabfaca1
xogml123 <xogml951@gmail.com>
2021-11-15 07:11:02 +0900

    Add Interface and class and inheritance and static and access modifier

--- typescript/typescript.js
+++ typescript/typescript.js
@@ -1,14 +1,54 @@
-"use strict";
-exports.__esModule = true;
-exports.forModule = void 0;
-/*
-숫자 두개를 매개변수로 받아 곱을 리턴하는 함수를 만들고 싶다.
-*/
-exports.forModule = 4;
+//Type Annotation
+//exports.forModule=3;
 function multiply(x, y) {
     return x * y;
 }
-var numbers = multiply(3, 6);
+let numbers = multiply(3, 6);
 //const strings = multiply('a', 'b');
 console.log(numbers);
-//console.log(strings);
+class Employee {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+        Employee.employeeNumber++;
+    }
+    evaluateSalary() {
+        return this.name.length * 10000 + this.gender.length * 50000;
+    }
+    static getEmployeeNumber() {
+        //static method
+        return Employee.employeeNumber;
+    }
+}
+Employee.employeeNumber = 0; //static property
+const employeeUsingObject = {
+    name: 'Tom',
+    gender: 'male',
+    evaluateSalary: function () {
+        return this.name.length * 10000 + this.gender.length * 50000;
+    },
+}; //Employee 생성자로 호출되지 않음.
+const employeeUsingClass = new Employee('Sharon', 'female');
+console.log(`employee name:  ${employeeUsingClass.name} 
+employee gender:${employeeUsingClass.gender}
+employee number: ${Employee.getEmployeeNumber()}`);
+//Class
+class Boss extends Employee {
+    constructor(name, gender, stockPercentage) {
+        super(name, gender);
+        this.stockPercentage = stockPercentage;
+    }
+    evaluateSalary() {
+        //overriding
+        return 100000000;
+    }
+    getStockPercentage() {
+        return this.stockPercentage;
+    }
+}
+const boss = new Boss('Brian', 'male', 20);
+console.log(`boss name: ${boss.name}
+boss gender: ${boss.gender}
+boss salary: ${boss.evaluateSalary()}
+employee number: ${Employee.getEmployeeNumber()}
+stockPercentage: ${boss.getStockPercentage()}`);
--- typescript/typescript.ts
+++ typescript/typescript.ts
@@ -1,13 +1,80 @@
-/*
-숫자 두개를 매개변수로 받아 곱을 리턴하는 함수를 만들고 싶다.
-*/
-export const forModule = 4;
+//Type Annotation
+//exports.forModule=3;
+export const forModule = 3;
 
 function multiply(x: number, y: number) {
-  return x * y;
+    return x * y;
 }
 
 let numbers = multiply(3, 6);
 //const strings = multiply('a', 'b');
 console.log(numbers);
 //console.log(strings);
+
+//Interfaces
+interface IPerson {
+    name: string;
+    gender: string;
+}
+
+interface IEmployee extends IPerson {
+    evaluateSalary(): number;
+}
+
+class Employee implements IEmployee {
+    name: string;
+    gender: string;
+    private static employeeNumber: number = 0; //static property
+    constructor(name: string, gender: string) {
+        this.name = name;
+        this.gender = gender;
+        Employee.employeeNumber++;
+    }
+    evaluateSalary(): number {
+        return this.name.length * 10000 + this.gender.length * 50000;
+    }
+    static getEmployeeNumber(): number {
+        //static method
+        return Employee.employeeNumber;
+    }
+}
+
+const employeeUsingObject: IEmployee = {
+    name: 'Tom',
+    gender: 'male',
+    evaluateSalary: function (): number {
+        return this.name.length * 10000 + this.gender.length * 50000;
+    },
+}; //Employee 생성자로 호출되지 않음.
+
+const employeeUsingClass: IEmployee = new Employee('Sharon', 'female');
+
+console.log(
+    `employee name:  ${employeeUsingClass.name} 
+employee gender:${employeeUsingClass.gender}
+employee number: ${Employee.getEmployeeNumber()}`,
+);
+
+//Class
+class Boss extends Employee {
+    //inheritance
+    private stockPercentage; //private
+    constructor(name: string, gender: string, stockPercentage: number) {
+        super(name, gender);
+        this.stockPercentage = stockPercentage;
+    }
+    evaluateSalary(): number {
+        //overriding
+        return 100000000;
+    }
+    getStockPercentage(): number {
+        return this.stockPercentage;
+    }
+}
+
+const boss = new Boss('Brian', 'male', 20);
+console.log(`boss name: ${boss.name}
+boss gender: ${boss.gender}
+boss salary: ${boss.evaluateSalary()}
+employee number: ${Employee.getEmployeeNumber()}
+stockPercentage: ${boss.getStockPercentage()}`);
